a44006d5ce2629637331eb5fd246f75d8547f55d
shashwatrajhack <er.shashwatraj@gmail.com>
2024-05-29 21:38:26 +0530

    recursion concept

---
+++ Recursion/printName.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void name(int i, int n){
+    if(i>n)
+      return;
+    cout<<"raj"<<endl;
+    name(i+1,n);
+}
+
+int main(){
+    int n;
+    cin>>n;
+
+    name(1,n);
+
+    return 0;
+}
