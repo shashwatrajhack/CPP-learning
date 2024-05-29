b2161e5d9860e3f7dcea1f2997d59e0393065834
shashwatrajhack <er.shashwatraj@gmail.com>
2024-05-29 23:15:09 +0530

    print no without loop

--- Recursion/sum.cpp
+++ Recursion/sum.cpp
@@ -1,9 +1,32 @@
-#include <iostream>
+#include <bits/stdc++.h>
 
 using namespace std;
 
+
+class Solution{
+    public:
+
+    void printNos(int N){
+        if(N>=1){
+            printNos(N-1);
+            cout<<N<<" ";
+
+        }
+        return;
+    }
+};
+
 int main() {
     // Your code here
-    cout << "Hello, world!" << endl;
+    int t;
+    cin>>t;
+    while(t--){
+        int N;
+        cin>>N;
+        Solution ob;
+        ob.printNos(N);
+        cout<<" \n";
+    }
+    
     return 0;
 }
