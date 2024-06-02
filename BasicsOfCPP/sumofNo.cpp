36d78d4f714b1046c4ed1a6840f3aa7231347403
shashwatrajhack <er.shashwatraj@gmail.com>
2024-06-02 19:45:55 +0530

    sum of n no.

---
+++ BasicsOfCPP/sumofNo.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+void solve(int n){
+    int sum = 0;
+    for(int i = 1;i<=n;i++){
+        sum +=i;
+        
+
+    }
+    cout<<sum<<endl;
+
+}
+
+int main() {
+    // Your code here
+    solve(6);
+    solve(10);
+    return 0;
+}
