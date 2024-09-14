3af1843d270c315e8d3e17d01c4706dccb7aa0ed
shashwatrajhack <er.shashwatraj@gmail.com>
2024-09-14 12:59:37 +0530

    Array operation

---
+++ Array/sorted.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdC++.h>
+
+using namespace std;
+
+int sortArr(vector<int>& arr){
+    sort(arr.begin(),arr.end());
+    return arr[arr.size()-1];
+}
+
+int main() {
+    // Your code here
+    cout << "Hello, world!" << endl;
+    
+
+    return 0;
+}
