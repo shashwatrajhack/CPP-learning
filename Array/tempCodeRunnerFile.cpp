c508a491c3668bd73db0461c787e6ed46e86a662
shashwatrajhack <er.shashwatraj@gmail.com>
2025-01-29 21:26:09 +0530

    temporary files

--- Array/tempCodeRunnerFile.cpp
+++ Array/tempCodeRunnerFile.cpp
@@ -1,12 +1,13 @@
-void solve(int arr[], int n) {
-//   int temp[n];
-//   for (int i = 1; i < n; i++) {
-//     temp[i - 1] = arr[i];
-//   }
-//   temp[n - 1] = arr[0];
-//   for (int i = 0; i < n; i++) {
-//     cout << temp[i] << " ";
-//   }
-//   cout << endl;
+void solve(int arr[], int n)
+ {
+  int temp[n];
+  for (int i = 1; i < n; i++) {
+    temp[i - 1] = arr[i];
+  }
+  temp[n - 1] = arr[0];
+  for (int i = 0; i < n; i++) {
+    cout << temp[i] << " ";
+  }
+  cout << endl;
 
-// }
+}
